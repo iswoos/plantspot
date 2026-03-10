68c06192892bd91840a928f404a90b00918352b5
이승우 <swlee@joara.com>
2026-03-10 10:48:40 +0900

    Fix : 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.studio.plantspot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
